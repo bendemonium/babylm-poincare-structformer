aec15c410e9b2c7863dab6c96efc8c99e8f878bd
Ridhi Bandaru <ridhib2422@gmail.com>
2025-08-02 01:42:58 -0500

    setup for lambda

--- setup_lambda.sh
+++ setup_lambda.sh
@@ -4,35 +4,42 @@
 # ./setup_lambda.sh
 
 
-set -e
+# set -e
 
-sudo apt install -y software-properties-common
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install -y python3.11 python3.11-venv python3.11-dev
+# sudo apt install -y software-properties-common
+# sudo add-apt-repository -y ppa:deadsnakes/ppa
+# sudo apt update
+# sudo apt install -y python3.11 python3.11-venv python3.11-dev
 
-python3.10 -m venv bb-env
-source bb-env/bin/activate
+# python3.10 -m venv bb-env
+# source bb-env/bin/activate
 
-pip install --upgrade pip
+# pip install --upgrade pip
 
 
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-pip uninstall numpy -y
-pip install numpy<2.0
+# pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# pip uninstall numpy -y
+# pip install numpy<2.0
 
-pip install -r requirements.txt
-# git clone https://github.com/bendemonium/babylm-poincare-structformer.git
+# pip install -r requirements.txt
+# # git clone https://github.com/bendemonium/babylm-poincare-structformer.git
 
-export PYTHONPATH=/home/ubuntu/babylm-poincare-structformer
+# export PYTHONPATH=/home/ubuntu/babylm-poincare-structformer
 
-huggingface-cli login
+# huggingface-cli login
 
-git config --global user.name "bendemonium"
-git config --global user.email "ridhib2422@gmail.com"
-git config --global credential.helper store
+# git config --global user.name "bendemonium"
+# git config --global user.email "ridhib2422@gmail.com"
+# git config --global credential.helper store
 
-git remote -v
+# git remote -v
 
-git add .
+# git add .
 
+
+#----- CHECKK CuDNN VERSION -----#
+
+# source bb-env/bin/activate
+# pip install --upgrade pip
+# pip install --upgrade "jax[cuda12]"
+# python -c "import jax; print(jax.__version__); print(jax.devices())"
